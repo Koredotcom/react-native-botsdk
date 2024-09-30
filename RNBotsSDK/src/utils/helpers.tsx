597fdfcc8a135836378aec2366fa9c5443a5c620
Sathish Challa <Challa.Sathish@kore.com>
2024-09-30 19:18:59 +0530

    Removed unused NPM packages and other configurations

--- RNBotsSDK/src/utils/helpers.tsx
+++ RNBotsSDK/src/utils/helpers.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// Correct: Import React from 'react'
-
 import {Dimensions, Platform, PixelRatio} from 'react-native';
 
 const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
@@ -8,43 +5,6 @@ const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
 let NEW_SCREEN_WIDTH =
   SCREEN_WIDTH > SCREEN_HEIGHT ? SCREEN_HEIGHT : SCREEN_WIDTH;
 const scale = NEW_SCREEN_WIDTH / 375;
-
-const ICON_SHAPE = {
-  CIRCLE_IMG: 'circle-img',
-};
-
-const imageTypes: any = [
-  'bmp',
-  'dds',
-  'gif',
-  'heic',
-  'jpg',
-  'png',
-  'psd',
-  'pspimage',
-  'tga',
-  'thm',
-  'tif',
-  'tiff',
-  'yuv',
-  'jpeg',
-];
-const videoTypes: any = [
-  '3g2',
-  '3gp',
-  'asf',
-  'avi',
-  'flv',
-  'm4v',
-  'mov',
-  'mp4',
-  'mpg',
-  'rm',
-  'srt',
-  'swf',
-  'vob',
-  'wmv',
-];
 
 export function normalize(size: number) {
   const newSize = size * scale;
