090f43afbfeabd09610267ed179d70a90d47337d
SudheerJa-Kore <sudheer.jampana@kore.com>
2025-04-09 19:31:14 +0530

    updating socket library

---
+++ RNBotsSDK/bot-sdk/model/BotConfigModel.tsx
@@ -0,0 +1,11 @@
+export interface BotConfigModel {
+  botName: string;
+  botId: string;
+  clientId: string;
+  clientSecret: string;
+  botUrl: string;
+  identity: string;
+  jwtServerUrl: string;
+  isWebHook: boolean;
+  value_aud: string;
+}
