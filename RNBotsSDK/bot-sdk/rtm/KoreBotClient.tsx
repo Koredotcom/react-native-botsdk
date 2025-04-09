b97facd1b6698a2fed92a557c1aee41e453ad120
SudheerJa-Kore <sudheer.jampana@kore.com>
2025-04-09 15:18:00 +0530

    kore bot sdk socket library

---
+++ RNBotsSDK/bot-sdk/rtm/KoreBotClient.tsx
@@ -0,0 +1,42 @@
+import {BotClient} from './BotClient';
+import {IBotClient} from './IBotClient';
+
+export class KoreBotClient extends BotClient implements IBotClient {
+  private static _instance: any;
+
+  private constructor() {
+    super();
+  }
+  public static getInstance(): IBotClient {
+    if (this._instance == null) {
+      this._instance = new KoreBotClient();
+    }
+    return this._instance;
+  }
+
+  public static disconnectClient() {
+    this._instance.disconnect();
+    this._instance = null;
+  }
+
+  setSessionActive(isActive: boolean) {
+    super.setSessionActive(isActive);
+  }
+
+  setAppState(_appState: string) {
+    super.setAppState(_appState);
+  }
+
+  setIsNetworkAvailable(isNetWorkAvailable: boolean) {
+    super.setIsNetworkAvailable(isNetWorkAvailable);
+  }
+
+  checkSocketAndReconnect() {
+    super.checkSocketAndReconnect();
+  }
+
+  getBotHistory() {
+    //Commented temporarily as it is not working
+    //super.getBotHistory();
+  }
+}
--- RNBotsSDK/index.js
+++ RNBotsSDK/index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './src/index';
+import App from './src/botIndex';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
