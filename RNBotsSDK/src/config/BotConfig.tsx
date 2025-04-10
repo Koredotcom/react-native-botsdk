18e195ac23f9227a55a26747ad6a703729b4c4d4
SudheerJa-Kore <sudheer.jampana@kore.com>
2025-04-10 13:25:16 +0530

    removed the static bot config details

--- RNBotsSDK/src/config/BotConfig.tsx
+++ RNBotsSDK/src/config/BotConfig.tsx
@@ -2,11 +2,12 @@ import uuid from 'react-native-uuid';
 import {BotConfigModel} from 'rn-kore-bot-socket-sdk';
 
 export const botConfig: BotConfigModel = {
-  botName: 'SDK V3 All Templates',
-  botId: 'st-027b5770-6ff0-5d9f-a3df-89254b012a55',
-  clientId: 'cs-c334ce38-b4ad-5027-861e-1d8150508bfa',
-  clientSecret: 'ItUgs7N4wN013LAxSxJlAKmj7K8uZnojBC7ZSCwpmZ4=',
-  botUrl: 'https://platform.kore.ai',
+  botName: 'PLEASE_ENTER_BOT_NAME',
+  botId: 'PLEASE_ENTER_BOT_ID',
+  clientId: 'PLEASE_ENTER_CLIENT_ID',
+  clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',
+  botUrl: 'PLEASE_ENTER_SERVER_URL',
+  jwtServerUrl: 'PLEASE_ENTER_JWT_SERVER_URL',
   identity: uuid.v4() + '',
   isWebHook: false,
   value_aud: 'https://idproxy.kore.com/authorize', //this is for jwt token generation
