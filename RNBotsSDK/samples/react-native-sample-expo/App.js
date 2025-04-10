e267f8152e290ee532bee2bebb57513e5a5f81ed
SudheerJa-Kore <sudheer.jampana@kore.com>
2025-04-10 13:12:54 +0530

    removed static bot config details

--- RNBotsSDK/samples/react-native-sample-expo/App.js
+++ RNBotsSDK/samples/react-native-sample-expo/App.js
@@ -9,15 +9,15 @@ export default function App() {
   const [resp, setResp] = useState('Your output shown here');
   const [title, setTitle] = useState('Connect');
   const botConfig = {
-    botName: 'SDK',
-    botId: 'st-b9889c46-218c-58f7-838f-73ae9203488c',
-    clientId: 'cs-1e845b00-81ad-5757-a1e7-d0f6fea227e9',
-    clientSecret: '5OcBSQtH/k6Q/S6A3bseYfOee02YjjLLTNoT1qZDBso=',
-    botUrl: 'https://bots.kore.ai',
-    jwtServerUrl: 'https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/',
+    botName: 'PLEASE_ENTER_BOT_NAME',
+    botId: 'PLEASE_ENTER_BOT_ID',
+    clientId: 'PLEASE_ENTER_CLIENT_ID',
+    clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',
+    botUrl: 'PLEASE_ENTER_SERVER_URL',
+    jwtServerUrl: 'PLEASE_ENTER_JWT_SERVER_URL',
     identity: uuid.v4() + '',
     isWebHook: false,
-    value_aud: 'https://idproxy.kore.com/authorize',
+    value_aud: 'https://idproxy.kore.com/authorize', //this is for jwt token generation
   };
   function onPressAction(){
     if(KoreBotClient.getInstance().getConnectionState() === ConnectionState.CONNECTED){
