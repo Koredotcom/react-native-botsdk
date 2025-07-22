8e33ca61d90babf74f1e4e69d886a73a5efc5dac
SudheerJa-Kore <sudheer.jampana@kore.com>
2025-07-22 14:31:07 +0530

    updated the config

--- RNKoreBotSDK/SampleApp/src/config/BotConfig.tsx
+++ RNKoreBotSDK/SampleApp/src/config/BotConfig.tsx
@@ -15,16 +15,16 @@ export const botConfig_f: BotConfigModel = {
   isFooterVisible: true
 };
 
-export const botConfig: BotConfigModel = {
-  botName: 'SDK V3 All Templates',
+export const botConfig : BotConfigModel = {
+  botName: 'PLEASE_ENTER_BOT_NAME',
   botId: 'PLEASE_ENTER_BOT_ID',
-  clientId: 'PLEASE_ENTER_CLIENT_ID',,
-  clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',,
-  botUrl: 'https://platform.kore.ai',
+  clientId: 'PLEASE_ENTER_CLIENT_ID',
+  clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',
+  botUrl: 'PLEASE_ENTER_SERVER_URL', // Should not end with '/', Example :  https://your.server.url
+  jwtServerUrl: 'PLEASE_ENTER_JWT_SERVER_URL',
   identity: uuid.v4(),
   isWebHook: false,
-  value_aud: 'https://idproxy.kore.com/authorize', //this is for jwt token generation
-  jwtServerUrl: 'https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/',
+  value_aud: 'https://idproxy.kore.com/authorize',
   isHeaderVisible: true,
-  isFooterVisible: true,
+  isFooterVisible: true
 };
