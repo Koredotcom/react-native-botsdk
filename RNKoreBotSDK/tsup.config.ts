3d4d69d8c602818e2e8b2cdb66cd2b4937d944a2
SudheerJa-Kore <sudheer.jampana@kore.com>
2025-07-25 19:28:54 +0530

    Version updated and updated read me and v

--- RNKoreBotSDK/tsup.config.ts
+++ RNKoreBotSDK/tsup.config.ts
@@ -4,7 +4,7 @@ import inlineImage from 'esbuild-plugin-inline-image';
 export default defineConfig({
   format: ['cjs', 'esm'],
   entry: ['./src/index.tsx'],
-  dts: true,
+  dts: false,
   shims: true,
   skipNodeModulesBundle: true,
   clean: true,
