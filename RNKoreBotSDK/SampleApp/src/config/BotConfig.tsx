0cd07c1692e695f87f46fe4d0d03f42369717afc
SudheerJa-Kore <sudheer.jampana@kore.com>
2025-08-22 12:43:14 +0530

    Revert lazy loading for communications and update SampleApp configurations

--- RNKoreBotSDK/SampleApp/src/config/BotConfig.tsx
+++ RNKoreBotSDK/SampleApp/src/config/BotConfig.tsx
@@ -1,5 +1,4 @@
-import { uuid } from 'rn-kore-bot-sdk-v77';
-import {BotConfigModel} from 'rn-kore-bot-socket-lib-v77';
+import { BotConfigModel } from 'rn-kore-bot-socket-lib-v77';
 
 export const botConfig_f: BotConfigModel = {
   botName: 'PLEASE_ENTER_BOT_NAME',
@@ -8,7 +7,7 @@ export const botConfig_f: BotConfigModel = {
   clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',
   botUrl: 'PLEASE_ENTER_SERVER_URL', // Should not end with '/', Example :  https://your.server.url
   jwtServerUrl: 'PLEASE_ENTER_JWT_SERVER_URL',
-  identity: uuid.v4(),
+  identity: '1234567890',
   isWebHook: false,
   value_aud: 'https://idproxy.kore.com/authorize',
   isHeaderVisible: true,
@@ -16,13 +15,13 @@ export const botConfig_f: BotConfigModel = {
 };
 
 export const botConfig: BotConfigModel = {
-  botName: 'PLEASE_ENTER_BOT_NAME',
-  botId: 'PLEASE_ENTER_BOT_ID',
-  clientId: 'PLEASE_ENTER_CLIENT_ID',
-  clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',
-  botUrl: 'PLEASE_ENTER_SERVER_URL', // Should not end with '/', Example :  https://your.server.url
-  jwtServerUrl: 'PLEASE_ENTER_JWT_SERVER_URL',
-  identity: uuid.v4(),
+  botName: 'SDK V3 All Templates',
+  botId: 'st-027b5770-6ff0-5d9f-a3df-89254b012a55',
+  clientId: 'cs-c334ce38-b4ad-5027-861e-1d8150508bfa',
+  clientSecret: 'ItUgs7N4wN013LAxSxJlAKmj7K8uZnojBC7ZSCwpmZ4=',
+  botUrl: 'https://platform.kore.ai',
+  jwtServerUrl: 'https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/',
+  identity: '1234567890',
   isWebHook: false,
   value_aud: 'https://idproxy.kore.com/authorize',
   isHeaderVisible: true,
