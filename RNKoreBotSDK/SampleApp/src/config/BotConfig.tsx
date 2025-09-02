c80c9aaeb19d184e11862437274a84a1d596a610
SudheerJa-Kore <64408292+SudheerJa-Kore@users.noreply.github.com>
2025-09-02 16:38:33 +0530

    Update BotConfig.tsx

--- RNKoreBotSDK/SampleApp/src/config/BotConfig.tsx
+++ RNKoreBotSDK/SampleApp/src/config/BotConfig.tsx
@@ -15,12 +15,12 @@ export const botConfig_f: BotConfigModel = {
 };
 
 export const botConfig: BotConfigModel = {
-  botName: 'SDK V3 All Templates',
+   botName: 'PLEASE_ENTER_BOT_NAME',
   botId: 'PLEASE_ENTER_BOT_ID',
-  clientId: 'PLEASE_ENTER_CLIENT_ID',,
-  clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',,
-  botUrl: 'https://platform.kore.ai',
-  jwtServerUrl: 'https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/',
+  clientId: 'PLEASE_ENTER_CLIENT_ID',
+  clientSecret: 'PLEASE_ENTER_CLIENT_SECRET',
+  botUrl: 'PLEASE_ENTER_SERVER_URL', // Should not end with '/', Example :  https://your.server.url
+  jwtServerUrl: 'PLEASE_ENTER_JWT_SERVER_URL',
   identity: '1234567890',
   isWebHook: false,
   value_aud: 'https://idproxy.kore.com/authorize',
