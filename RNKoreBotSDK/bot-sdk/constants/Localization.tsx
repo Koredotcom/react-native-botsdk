ae7901385ae1f1d6e3737b2a71564e14d27cc2c9
KishoreTA-Kore <kishore.talloju@kore.com>
2025-09-26 12:47:33 +0530

    Fixed issue in 'Implement Pull-to-Refresh to Load Previous Chat Content'

--- RNKoreBotSDK/bot-sdk/constants/Localization.tsx
+++ RNKoreBotSDK/bot-sdk/constants/Localization.tsx
@@ -47,7 +47,8 @@ const LOCALIZED_STRINGS = {
     minimize: 'MINIMIZE',
     view_more: 'View More',
     answered_by_ai: 'Answered by AI',
-    did_not_get_code: 'Didn\'t get a code?'
+    did_not_get_code: 'Didn\'t get a code?',
+    load_earlier_messages: 'Load Earlier Messages'
   },
   es: {
     back_dialog_title: '¿Te gustaría cerrar la conversación o minimizar?',
